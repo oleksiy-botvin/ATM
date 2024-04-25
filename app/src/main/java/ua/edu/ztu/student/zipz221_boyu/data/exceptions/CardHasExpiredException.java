1d26d36e3c2af6d68276ad6510b3d3da12e7325c
botvin_oleksiy <zipz221_boyu@student.ztu.edu.ua>
2024-04-25 21:41:18 +0300

    added new exceptions

---
+++ app/src/main/java/ua/edu/ztu/student/zipz221_boyu/data/exceptions/CardHasExpiredException.java
@@ -0,0 +1,7 @@
+package ua.edu.ztu.student.zipz221_boyu.data.exceptions;
+
+public class CardHasExpiredException extends Exception {
+    public CardHasExpiredException() {
+        super();
+    }
+}
---
+++ app/src/main/java/ua/edu/ztu/student/zipz221_boyu/data/exceptions/RanOutOfMoneyException.java
@@ -0,0 +1,8 @@
+package ua.edu.ztu.student.zipz221_boyu.data.exceptions;
+
+public class RanOutOfMoneyException extends Exception {
+
+    public RanOutOfMoneyException() {
+
+    }
+}
